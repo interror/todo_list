744f0eae148ab16d9c2705c2d2c6c991bbf2370d
john_denisov <rock.the.ruby@gmail.com>
2013-08-04 11:21:51 +0300

    Add create action for tasks

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,11 @@
 class ProjectsController < ApplicationController
 	def index
 		@projects=Project.all.to_a
+		@new_task = Task.new
+	end
+
+	def destroy
+		Project.find(params[:id]).destroy
+		redirect_to root_url
 	end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,22 @@
 class TasksController < ApplicationController
+
+	def show
+
+	end
+
 	def destroy
-		@task.destroy
+		Task.find(params[:id]).destroy
 		redirect_to root_url
 	end
+
+	def create
+		Task.create!(task_params)
+		redirect_to root_url
+	end
+
+	private 
+		def task_params
+			params.require(:task).permit(:title, :project_id, :deadline)
+		end
+
 end
